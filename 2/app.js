c4e0b3a7310f0ba7e9125780754d6c224329f19a
iliya <iliyacaspi@gmail.com>
2022-02-04 19:14:44 +0200

    all commit one

--- 2/app.js
+++ 2/app.js
@@ -1,15 +1,11 @@
-//1
-//2
-//3
-//4
 localStorage.clear();
 console.log(localStorage);
 const diceFaces = [1,2,3,4,5,6];
 
 let container = document.getElementById('container');
-let dice;
+let dice = document.getElementById('dice-1');
 let rollBtn = document.getElementById('btn');
-let numOfDice = document.getElementById('num').value;
+/* let numOfDice = document.getElementById('num').value; */
 let sound1 = new Audio("sounds/DICE.mp3");
 
 
@@ -23,18 +19,43 @@ function randomRoll(i){
                 randomRoll(i);        
     }else{
         dice[i].firstElementChild.setAttribute('CLASS',`rotate-${result}`);
-        sound1.currentTime = 0 ;
-        sound1.play(); 
+        /* sound1.currentTime = 0 ;
+        sound1.play();  */
     }    
 }
 
-rollBtn.addEventListener('click',function(){
+/* rollBtn.addEventListener('click',function(){
     let spark = document.getElementById('spark');
     spark.style.display='block';
     setTimeout(()=>{
         spark.style.display='none';
     },400)
+}) */
+let rtationX = 0;
+let rtationY = 0;
+let rtationZ = 0;
+
+dice.addEventListener('mousemove',function(e){
+    
+    /* console.log(e.target);
+    console.log(e.target.parentElement);
+    console.log(e.target.parentElement.childElementCount);
+    console.log(e.target.parentElement.childNodes.length); */   
+    if(e.movementX>0){
+        rtationX++;
+        e.target.parentElement.style.transform = `rotateX(${rtationX}deg)`;
+    }
+    else if(e.movementX<0){
+        rtationX--;
+        e.target.parentElement.style.transform = `rotateX(${rtationX}deg)`;
+    }
+
 })
+
+function rando6(num){
+    let randomNum = Math.floor(Math.random()*num);
+    return randomNum;
+}
 
 function roll(){
     for(let i=0; i<numOfDice; i++){ 
@@ -62,7 +83,7 @@ function createStage(){
     }
 
     container.innerHTML=snippet;
-    dice = document.querySelectorAll('.dice-container');
+    /* dice = document.querySelectorAll('.dice-container'); */
                                                                     
     setTimeout(function(){
         roll();
